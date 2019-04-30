0153731e9fab67c2ef6b7e6bcd78343285f40d40
Andy Gray <andy@whatsthescore.co.uk>
2019-04-30 17:34:09 +0100

    more

--- functions/scripts/name-process.js
+++ functions/scripts/name-process.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
 
 void async function () {
-    const data = require('./data/54_firstname');
+    const data = require('./data/54_lastname');
 
     console.log(
         _.reduce(data, (obj, v, i) => {
